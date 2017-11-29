0217b8050016fd5bed2df58917a4831b06f8e624
Jawann <jawann@gmail.com>
2017-11-29 02:17:35 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |keys, values|
     puts "#{keys.capitalize}:"
     values.each do |key, value|
-      puts "  #{key.capitalize}: #{value}"
+      puts "  #{key.capitalize}: #{value.join(", ")}"
     end
   end
 end
